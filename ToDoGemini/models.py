89ce016916bd954070e8d4b895e786fabdef46ca
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-02-20 16:36:22 +0300

    Dependency Injection kodları eklendi

--- ToDoGemini/models.py
+++ ToDoGemini/models.py
@@ -7,5 +7,6 @@ class ToDo(Base):
     id=Column(Integer,primary_key=True,index=True)
     title=Column(String)
     description=Column(String)
-    completed=Column(Boolean,default=False)
+    priority=Column(Integer)
+    complete=Column(Boolean,default=False)
 
